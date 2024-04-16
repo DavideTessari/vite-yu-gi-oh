dbccd85c91f557ff587d040ec01219882f3d3076
DavideTessari <davtex@gmail.com>
2024-04-16 17:35:21 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,92 +2,56 @@
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import CharactersList from './components/CharactersList.vue';
-import CharacterCard from './CharacterCard.vue';
+import AppSearch from './components/AppSearch.vue';
 import AppLoader from './components/AppLoader.vue';
 import { store } from './store.js';
 
 export default {
-  name: 'CharactersList',
   components: {
-    CharacterCard
+    AppHeader,
+    CharactersList,
+    AppLoader,
+    AppSearch
   },
   data() {
     return {
       store,
-      archetypes: [],
-      selectedArchetype: '',
+      archetypes: []
     };
   },
-  computed: {
-    filteredCards() {
-      // Filtra le carte in base all'archetipo selezionato
-      if (!this.selectedArchetype) {
-        return this.store.cards;
-      } else {
-        return this.store.cards.filter(card => card.archetype === this.selectedArchetype);
-      }
-    }
-  },
   methods: {
-    fetchArchetypes() {
-      // Ottieni la lista degli archetipi dall'API
+    getArchetypeFromApi() {
       axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
         .then(response => {
           this.archetypes = response.data.data;
         });
     },
-    filterByArchetype() {
-      // Chiamata API per filtrare le carte per archetipo selezionato
-      if (this.selectedArchetype) {
-        axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${this.selectedArchetype}&num=20&offset=0`)
-          .then(response => {
-            this.store.cards = response.data.data;
-          });
-      } else {
-        // Se non è selezionato un archetipo, ripristina tutte le carte
-        this.getCardFromApi();
-      }
-    },
     getCardFromApi() {
       axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
         .then(response => {
-          this.store.cards = response.data.data;
+          this.store.cards = response.data.data; 
           this.store.isLoading = false;
         });
     }
   },
   mounted() {
-    this.fetchArchetypes();
-    this.getCardFromApi(); 
+    this.getCardFromApi();
+    this.getArchetypeFromApi();
   }
 };
 </script>
 
 <template>
-  <section class="characters-list">
-    <div class="filter-container">
-      <label for="archetypeSelect">Filtra per Archetipo:</label>
-      <select id="archetypeSelect" v-model="selectedArchetype" @change="filterByArchetype">
-        <option value="">Tutti gli Archetipi</option>
-        <option v-for="archetype in archetypes" :key="archetype" :value="archetype">{{ archetype }}</option>
-      </select>
-    </div>
-    <div class="container">
-      <div class="characters-cards">
-        <CharacterCard v-for="card in filteredCards" :key="card.id" :data="card" />
-      </div>
-    </div>
-  </section>
+  <div>
+    <AppHeader />
+    <main>
+      <AppSearch @searchPerformed="getArchetypeFromApi"></AppSearch>
+      <CharactersList v-if="!store.isLoading" />
+      <AppLoader v-else />
+    </main>
+  </div>
 </template>
 
-<style scoped lang="scss">
-.characters-list {
-  .filter-container {
-    margin-bottom: 20px;
-  }
-  .characters-cards {
-    display: flex;
-    flex-wrap: wrap;
-  }
-}
+<style lang="scss">
+@use './style/generic';
 </style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,39 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    name: 'AppSearch',
+    data() {
+        return {
+            store,
+        };
+    }
+}
+</script>
+
+<template>
+    <section class="app-search">
+        <div class="container">
+            
+            <select v-model="store.archetype">
+                <option value="">All Archetype</option>
+                <!-- Scelte disponibili -->
+                <option @click="$emit('searchPerfomed')"></option>
+            </select>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+
+.app-search {
+    padding: 20px 0;
+
+    input,
+    select {
+        padding: 10px 12px;
+        font-size: 22px;
+        margin-right: 10px;
+    }
+}
+</style>
